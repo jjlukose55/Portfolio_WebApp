14cfe6e24cd7378e4b2d9b82a502e7d884cbad57
Justin Lukose <jjlukose55@gmail.com>
2025-10-01 09:37:35 -0400

    mobile changes

--- mobile/app.js
+++ mobile/app.js
@@ -1,5 +1,16 @@
-// SPA Dynamic Page Loader
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Script is running");
-    
+// Style Variables (Used for dynamic theming)
+const tiles = ['--tile1', '--tile2', '--tile3']; 
+const signature = document.getElementById('signature');
+
+// Code for signature tile change
+let currentTile = parseInt(localStorage.getItem('currentTile')) || 0; // Get saved tile index or default to 0
+// Set the initial background tile from localStorage
+document.documentElement.style.setProperty('--bgTile', `var(${tiles[currentTile]})`);
+
+signature.addEventListener('click', () => {
+    // Change the background tile
+    currentTile = (currentTile + 1) % tiles.length; // Cycle to the next tile
+    document.documentElement.style.setProperty('--bgTile', `var(${tiles[currentTile]})`);
+    // Save the current tile index in localStorage
+    localStorage.setItem('currentTile', currentTile);
 });
